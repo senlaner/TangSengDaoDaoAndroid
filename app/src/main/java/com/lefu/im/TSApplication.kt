083e609d592897fda8a88b8d4fe499182730f726
胡雄飞 <huxiongfei>
2025-06-24 17:51:28 +0800

    更换服务端地址

--- app/src/main/java/com/lefu/im/TSApplication.kt
+++ app/src/main/java/com/lefu/im/TSApplication.kt
@@ -108,7 +108,7 @@ class TSApplication : MultiDexApplication() {
     private fun initApi() {
         var apiURL = WKSharedPreferencesUtil.getInstance().getSP("api_base_url")
         if (TextUtils.isEmpty(apiURL)) {
-            apiURL = "https://api.botgate.cn"
+            apiURL = "http://lefu.me:8090"
             WKApiConfig.initBaseURL(apiURL)
         } else {
             WKApiConfig.initBaseURLIncludeIP(apiURL)
